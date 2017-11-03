98bbe2b15f2dd8d12f2e8690b1b60e0708e9a72a
gempir <gempir.dev@gmail.com>
2017-11-03 22:13:35 +0100

    init with simple tabs and a webView

---
+++ app/src/main/java/com/gempir/chattix/Login.kt
@@ -0,0 +1,18 @@
+package com.gempir.chattix
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_login.*
+import android.webkit.WebView
+
+
+
+class Login : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+}
